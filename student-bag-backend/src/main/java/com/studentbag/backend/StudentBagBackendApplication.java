48054c1a1a0ce747953fd2eef2d7da07e8b49a17
𝙰𝚈𝙼𝙰𝙽 𝙰𝙻𝙹𝙰𝙼𝙰𝙻 <ayman.aljamal2017@gmail.com>
2026-03-10 13:35:18 +0200

    first commit

---
+++ student-bag-backend/src/main/java/com/studentbag/backend/StudentBagBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studentbag.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentBagBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentBagBackendApplication.class, args);
+	}
+
+}
---
+++ student-bag-backend/src/test/java/com/studentbag/backend/StudentBagBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentbag.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentBagBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
